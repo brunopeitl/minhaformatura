e7b88a4829f94cc1422a32f421cbe4242f272428
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2018-11-23 20:55:43 -0200

    Update testedb.js

--- www/js/testedb.js
+++ www/js/testedb.js
@@ -34,76 +34,13 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
 	
-	
-	//----------- Aqui vai uma parte das coisas
-	document.addEventListener('deviceready', onDeviceReady, false);
-	var logOb;
-	function fail(e) {
-		console.log("FileSystem Error");
-		console.dir(e);
-	}
-	//-----------
-	
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
 	//---------------------------Aqui vai tudo o que interessa
-	//document.getElementById("teste").innerHTML = quantidade[0];
-	//-------------------------------- A Coisa toda vai aqui
-
-		window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
-			console.log("got main dir",dir);
-			dir.getFile("log.txt", {create:true}, function(file) {
-				console.log("got the file", file);
-				logOb = file;
-				writeLog("App started");			
-			});
-		});
-	
-		document.querySelector("#actionOne").addEventListener("touchend", function(e) {
-			//Ok, normal stuff for actionOne here
-			//
-			//Now log it
-			writeLog("actionOne fired");
-		}, false);
-
-		document.querySelector("#actionTwo").addEventListener("touchend", function(e) {
-			//Ok, normal stuff for actionTwo here
-			//
-			//Now log it
-			writeLog("actionTwo fired");
-		}, false);
-
-	}
-
-	function writeLog(str) {
-		if(!logOb) return;
-		var log = str + " [" + (new Date()) + "]\n";
-		console.log("going to log "+log);
-		logOb.createWriter(function(fileWriter) {
-		
-			fileWriter.seek(fileWriter.length);
-		
-			var blob = new Blob([log], {type:'text/plain'});
-			fileWriter.write(blob);
-			console.log("ok, in theory i worked");
-		}, fail);
-	}
-
-	function justForTesting() {
-		logOb.file(function(file) {
-			var reader = new FileReader();
-
-			reader.onloadend = function(e) {
-				console.log(this.result);
-			};	
-
-			reader.readAsText(file);
-		}, fail);
-
-	//-------------------------------- Aqui termina a coisa toda
+	document.getElementById("teste").innerHTML = quantidade[0];
 	//---------------------------------------------------------
 		
         var parentElement = document.getElementById(id);
