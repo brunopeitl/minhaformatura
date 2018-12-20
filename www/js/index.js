29a2a69749eca54d40e97e8c23e4477c21e0e147
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2018-12-20 13:41:34 -0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -76,9 +76,9 @@ var app = {
 		$(document).ready(function(){
 			//---ANIMAÇÃO
 			$("#logo_entrada").fadeIn(1500, function(){
-				$("#logo_entrada").delay(1500).fadeOut(1500, function(){ 
-					document.getElementById("fundo_entrada").style.display = "none";
+				$("#logo_entrada").delay(1500).fadeOut(1500, function(){
 					alert(baixado);
+					document.getElementById("fundo_entrada").style.display = "none";
 					document.getElementById("pagina_login").style.display = "block";
 				});
 			})
