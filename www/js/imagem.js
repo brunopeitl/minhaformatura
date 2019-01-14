9513a7f1a7acaa3c508ccbe9cf516d45dbfb6a7f
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2019-01-14 20:10:00 -0200

    Update imagem.js

--- www/js/imagem.js
+++ www/js/imagem.js
@@ -39,10 +39,17 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
 	//Aqui vai a minha programação
-	var camImagem1 = cordova.file.dataDirectory+"imagens/001.jpg";
-	var camImagem2 = cordova.file.dataDirectory+"imagens/002.jpg";
-	var camImagem3 = cordova.file.dataDirectory+"imagens/05.jpg";
-	document.getElementById("imagem").innerHTML = "<img src='"+camImagem1+"' /><br /><img src='"+camImagem2+"' /><br /><img src='"+camImagem3+"' />";
+	var camImagem1 = cordova.file.dataDirectory+"imagens/CAPA.jpg";
+	var camImagem2 = cordova.file.dataDirectory+"imagens/001.jpg";
+	var camImagem3 = cordova.file.dataDirectory+"imagens/002.jpg";
+	var camImagem4 = cordova.file.dataDirectory+"imagens/003.jpg";
+	var camImagem5 = cordova.file.dataDirectory+"imagens/004.jpg";
+	var camImagem6 = cordova.file.dataDirectory+"imagens/005.jpg";
+	var camImagem7 = cordova.file.dataDirectory+"imagens/006.jpg";
+	var camImagem8 = cordova.file.dataDirectory+"imagens/007.jpg";
+	var camImagem9 = cordova.file.dataDirectory+"imagens/008.jpg";
+	var camImagem10 = cordova.file.dataDirectory+"imagens/009.jpg";
+	document.getElementById("imagem").innerHTML = "<img src='"+camImagem1+"' /><br /><img src='"+camImagem2+"' /><br /><img src='"+camImagem3+"' /><br /><img src='"+camImagem4+"' /><br /><img src='"+camImagem5+"' /><br /><img src='"+camImagem6+"' /><br /><img src='"+camImagem7+"' /><br /><img src='"+camImagem8+"' /><br /><img src='"+camImagem9+"' /><br /><img src='"+camImagem10+"' />";
 		
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
