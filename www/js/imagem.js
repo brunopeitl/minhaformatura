daa629258998f2e384da87d768c9b8a41ff9f4cb
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2019-02-04 11:19:06 -0200

    Update imagem.js

--- www/js/imagem.js
+++ www/js/imagem.js
@@ -16,7 +16,7 @@ function programacao() {
 	var imagemFundo = cordova.file.dataDirectory+"imagens/001.jpg";
 	document.getElementById("abertura_fundo").style.backgroundImage = "url('"+imagemFundo+"')";
 	
-	var myVar = setInterval(myTimer ,3);
+	var myVar = setInterval(myTimer ,1);
 	var w = 0;
 	var h = 0;
 	var myVar2 = setInterval(myTimer2 ,20);
