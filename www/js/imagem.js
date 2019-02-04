1d7bca84687b2c6bec8f3896b03307bbd214bf6b
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2019-02-04 00:56:35 -0200

    Update imagem.js

--- www/js/imagem.js
+++ www/js/imagem.js
@@ -1,22 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
- 
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -39,6 +21,7 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
 	//Aqui vai a minha programação
+	/*
 	var camImagem1 = cordova.file.dataDirectory+"imagens/CAPA.jpg";
 	var camImagem2 = cordova.file.dataDirectory+"imagens/001.jpg";
 	var camImagem3 = cordova.file.dataDirectory+"imagens/002.jpg";
@@ -50,7 +33,8 @@ var app = {
 	var camImagem9 = cordova.file.dataDirectory+"imagens/008.jpg";
 	var camImagem10 = cordova.file.dataDirectory+"imagens/009.jpg";
 	document.getElementById("imagem").innerHTML = "<img src='"+camImagem1+"' /><br /><img src='"+camImagem2+"' /><br /><img src='"+camImagem3+"' /><br /><img src='"+camImagem4+"' /><br /><img src='"+camImagem5+"' /><br /><img src='"+camImagem6+"' /><br /><img src='"+camImagem7+"' /><br /><img src='"+camImagem8+"' /><br /><img src='"+camImagem9+"' /><br /><img src='"+camImagem10+"' />";
-		
+	*/
+	
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
