6b409a4fbf170ffbeee80bb9d8bbf8b9ecf54c5c
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2019-05-16 19:18:02 -0300

    Update imagem.js

--- www/js/imagem.js
+++ www/js/imagem.js
@@ -6,6 +6,8 @@ listaFotos = new Array();
 
 //--- DECLARAÇÃO DE VARIÁVEIS
 //Variáveis e comandos para montar galeria
+var qtd_fotosArray = [];
+var qtd_fotos = qtd_fotosArray[0];
 //var qtd_fotos = listaFotos.length; //Quantidade de fotos
 
 //---------- VARIÁVEIS PARA MONTAR O LAYOUT RESPONSÍVEL DA TELA
@@ -167,7 +169,7 @@ function btComecar() {
 	document.getElementById("photo_unica").style.display = "block";
 	$("#photo_blur").fadeOut(1500, function(){
 		document.getElementById("photo_blur").style.display = "none";
-		var qtd_fotos = listaFotos.length;
+		var qtd_fotosArray[0] = listaFotos.length;
 		//Animação para desembaçar o fundo
 		/*
 		var myVar5 = setInterval(myTimer5 ,3);
