0a0feca669bc39ceb9bc637dc100eddfadef8089
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2019-05-28 08:29:05 -0300

    Update viewer.js

--- www/js/viewer.js
+++ www/js/viewer.js
@@ -1,41 +1,3 @@
-//Ler pasta onde estão as fotos baixadas e listar os arquivos contidos nela, inserindo na tag UL
-		function insereFotosArray() {
-			
-			alert("chegou até aqui");
-			
-			function listDir(path){
-				alert("chegou na etapa 01: "+path);
-				window.resolveLocalFileSystemURL(path,
-					alert("chegou na etapa 21: "+path);
-					function (fileSystem) {
-						var reader = fileSystem.createReader();
-						reader.readEntries(
-							function (entries) {
-								alert("quantidade: "+entries.length);
-								var i;
-								for (i=0; i<entries.length; i++) {
-									//listaFotos.push(entries[i].name);
-									/*if(i == entries.length - 1) {
-										geral();
-									}*/
-									//document.getElementById("pictures").innerHTML += "<li>Teste "+i+"</li>";
-									alert(i/*entries[i].name*/);
-									//document.getElementById("pictures").innerHTML += "<li><img data-original='"+path+entries[i].name"' src='"+path+entries[i].name+"' alt='Foto'"+i+"></li>";
-								}
-							},
-							function (err) {
-								console.log(err);
-							}
-						);
-						}, function (err) {
-							console.log(err);
-						}
-				);
-			}
-			//Chama a função passando o caminho da pasta na memória interna do aparelho a ser lida
-			listDir(cordova.file.dataDirectory + "imagens/");
-		}
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -57,13 +19,11 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-
-//----------------------------------------------------
+	    
 //Aqui vai a minha programação
-	
-  	alert("tá lendo o javascript pelo menos.");
-	insereFotosArray();
+//----------------------------------------------------
 
+	alert("JavaScript funcionando!");
 
 //FIM DA MINHA PROGRAMAÇÃO
 //----------------------------------------------------
