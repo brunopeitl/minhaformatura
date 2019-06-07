7f5ab1af85c62e71e3dbaf76a1dd7d86d8e9b869
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2019-06-07 10:10:24 -0300

    Create modal.js

---
+++ www/js/modal.js
@@ -0,0 +1,73 @@
+var conjuntoFotos = document.getElementById("conjuntoFotos");
+var fotos = ["img_5terre.jpg","img_forest.jpg","img_lights.jpg","img_mountains.jpg"];
+
+function teste() {
+  for (i = 0; i < fotos.length; i++) { 
+  	conjuntoFotos.innerHTML += "<div class=\"fotoUnitPaisagem\" style=\"background-image: url('" + fotos[i] + "')\" onclick=\"modalizar('" + fotos[i] + "')\"></div>";
+  }
+}
+
+
+function modalizar(numero) {
+	modal.style.display = "block";
+	modalImg.src = numero;
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+	    
+	//Aqui vai a minha programação
+	teste();
+	
+
+
+//FIM DA MINHA PROGRAMAÇÃO
+//----------------------------------------------------
+	    
+	    
+	
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+		
+    }
+};
