53b9a4f3c6ce0987ddcc70f818b3368e9ad8add4
brunopeitl <38541615+brunopeitl@users.noreply.github.com>
2019-06-12 10:11:12 -0300

    Update modal.js

--- www/js/modal.js
+++ www/js/modal.js
@@ -63,21 +63,11 @@ function zoomin() {
 		document.getElementsByClassName("modal-content")[0].style.height = "auto";
 		estadoZoom[0] = 2;
 	}
-	else if (estadoZoom[0] == 2) {
-		document.getElementsByClassName("modal-content")[0].style.width = "300%";
-		document.getElementsByClassName("modal-content")[0].style.height = "auto";
-		estadoZoom[0] = 3;
-	}
 	else {
 	}
 }
 function zoomout() {
-	if (estadoZoom[0] == 3) {
-		document.getElementsByClassName("modal-content")[0].style.width = "200%";
-		document.getElementsByClassName("modal-content")[0].style.height = "auto";
-		estadoZoom[0] = 2;
-	}
-	else if (estadoZoom[0] == 2) {
+	if (estadoZoom[0] == 2) {
 		document.getElementsByClassName("modal-content")[0].style.width = "150%";
 		document.getElementsByClassName("modal-content")[0].style.height = "auto";
 		estadoZoom[0] = 1;
